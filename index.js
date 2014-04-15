5d1203952bbda5b828c7aea7a69583ab48a7aeab
Avenger7x <avenger7x13@gmail.com>
2014-04-15 09:15:40 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+exports.formatters = {
+	upper: function(string){ return string.toUpperCase(); },
+	lower: function(string){ return string.toLowerCase(); }
+};
+
+exports.format = function format(formatters, string){
+	if(arguments.length < 2){
+		return format.bind(null, formatters);
+	}
+
+	return formatters.map(function(input){
+		return input.split(/\s+/);
+	}).filter(function(input){
+		return exports.formatters.hasOwnProperty(input[0]);
+	}).map(function(input){
+		return { fn: exports.formatters[input[0]], args: input.slice(1) };
+	}).reduce(function(string, formatter){
+		return formatter.fn(string, formatter.args);
+	}, string);
+};
---
+++ readme.md
@@ -0,0 +1,21 @@
+# String-pipeline
+
+
+
+## Install
+
+ * via [npm](https://npmjs.org) `npm install string-pipeline`
+ * via [component](http://component.io) `component install string-pipeline`
+ * via [bower](http://bower.io) `bower install string-pipeline`
+
+## API
+
+TBD
+
+## Usage
+
+TBD
+
+## License
+
+[MIT License](http://en.wikipedia.org/wiki/MIT_License) (c) Alexey Raspopov
